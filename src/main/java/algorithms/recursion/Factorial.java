693b3ca43b3f87820151c0430a7aff2faab472f9
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-03-20 21:49:15 +0200

    Factorial.java Fibonacci.java

--- src/main/java/algorithms/recursion/Factorial.java
+++ src/main/java/algorithms/recursion/Factorial.java
@@ -1,4 +1,19 @@
 package algorithms.recursion;
 
 public class Factorial {
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        int fact = factorial(N);
+        System.out.println(fact);
+    }
+
 }
--- src/main/java/algorithms/recursion/Fibonacci.java
+++ src/main/java/algorithms/recursion/Fibonacci.java
@@ -1,4 +1,15 @@
 package algorithms.recursion;
 
 public class Fibonacci {
+
+    static int fibo(int n) {
+        if (n <= 2) return 1;
+        return fibo(n - 1 ) + fibo (n - 2);
+    }
+
+    public static void main(String[] args) {
+        int number = 100;
+        int fibo10th = fibo(number);
+        System.out.println(fibo10th);
+    }
 }
