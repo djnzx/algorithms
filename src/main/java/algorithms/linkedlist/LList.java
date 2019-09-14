238eb32da00e06742d0d166dc0c9034a8b28258d
Alexey <alexey.rykhalskiy@gmail.com>
2019-09-14 14:40:37 +0300

    LList.java

--- src/main/java/algorithms/linkedlist/LList.java
+++ src/main/java/algorithms/linkedlist/LList.java
@@ -2,6 +2,9 @@ package algorithms.linkedlist;
 
 public class LList {
 
+    private Node head = null;
+    private Node tail = null;
+
     class Node {
         private final int val;
         private Node next = null;
@@ -18,12 +21,36 @@ public class LList {
             return next;
         }
 
+        public boolean hasNext() {
+            return next != null;
+        }
+
         public void setNext(Node next) {
             this.next = next;
         }
     }
 
+    private void add(int val) {
+        Node node = new Node(val);
+
+        if (head == null) {
+            head = node;
+        } else {
+            tail.setNext(node);
+        }
+        tail = node;
+
+    }
+
     private boolean contains(int val) {
+        Node current = head;
+        if (current == null) return false;
+
+        while (current != null) {
+            if (current.getVal() == val) return true;
+            current = current.getNext();
+        }
+
         return false;
     }
 
@@ -31,12 +58,22 @@ public class LList {
 
     }
 
-    private void add(int val) {
-
+    private void print_separator() {
+        System.out.print(", ");
     }
 
     private void print() {
-
+        Node current = head;
+        System.out.print("<");
+        if (current != null) {
+            System.out.print(current.getVal());
+            while (current.hasNext()) {
+                print_separator();
+                current = current.getNext();
+                System.out.print(current.getVal());
+            }
+        }
+        System.out.println(">");
     }
 
     public static void main(String[] args) {
@@ -49,6 +86,16 @@ public class LList {
         System.out.println(ll.contains(6)); // false
         ll.add(6);
         System.out.println(ll.contains(6)); // true
+
+//        for (Node el: ll) {
+//            System.out.println(el);
+//        }
+
+//        int[] a = {1,2,3};
+//        for (int i1: a) {
+//            System.out.println(a);
+//        }
+
         ll.print(); // <5,7,6>
         ll.remove(7);
         ll.print(); // <5,6>
