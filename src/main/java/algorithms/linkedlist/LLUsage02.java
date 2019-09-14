4e7b47804f9c7f086de90ce35373a4bb26e5ec87
Alexey <alexey.rykhalskiy@gmail.com>
2019-09-14 15:25:56 +0300

    LList.java

---
+++ src/main/java/algorithms/linkedlist/LLUsage02.java
@@ -0,0 +1,25 @@
+package algorithms.linkedlist;
+
+public class LLUsage02 {
+    public static void main(String[] args) {
+        LList ll1 = new LList();
+        LList ll2 = new LList();
+        LList ll3 = new LList();
+
+        ll1.add(1);
+        ll1.add(3);
+        ll1.add(10);
+
+        ll2.add(5);
+        ll2.add(6);
+        ll2.add(7);
+
+        ll1.print();
+        ll2.print();
+        // ========
+
+        // ========
+        ll3.print(); // <1, 3, 5, 6, 7, 10>
+
+    }
+}
--- src/main/java/algorithms/linkedlist/LList.java
+++ src/main/java/algorithms/linkedlist/LList.java
@@ -4,7 +4,11 @@ public class LList {
     private Node head = null;
     private Node tail = null;
 
-    private void add(int val) {
+    public Node getHead() {
+        return head;
+    }
+
+    public void add(int val) {
         Node node = new Node(val);
 
         if (head == null) {
@@ -16,7 +20,7 @@ public class LList {
 
     }
 
-    private boolean contains(int val) {
+    public boolean contains(int val) {
         Node current = head;
         if (current == null) return false;
 
@@ -28,7 +32,7 @@ public class LList {
         return false;
     }
 
-    private void remove(int val) {
+    public void remove(int val) {
         Node current = head;
         // empty list
         if (current == null) return;
@@ -61,7 +65,7 @@ public class LList {
         System.out.print(", ");
     }
 
-    private void print() {
+    public void print() {
         Node current = head;
         System.out.print("<");
         if (current != null) {
@@ -73,38 +77,6 @@ public class LList {
             }
         }
         System.out.println(">");
-    }
-
-    public static void main(String[] args) {
-        LList ll = new LList();
-        ll.print(); // <>
-        ll.add(5);
-        ll.add(7);
-        ll.print(); // <5,7>
-        System.out.println(ll.contains(5)); // true
-        System.out.println(ll.contains(6)); // false
-        ll.add(6);
-        System.out.println(ll.contains(6)); // true
-
-//        for (Node el: ll) {
-//            System.out.println(el);
-//        }
-
-//        int[] a = {1,2,3};
-//        for (int i1: a) {
-//            System.out.println(a);
-//        }
-
-        ll.print(); // <5,7,6>
-        ll.remove(7);
-        ll.print(); // <5,6>
-        ll.remove(6);
-        ll.print(); // <5>
-        ll.remove(5);
-        ll.print(); // <>
-        ll.add(77);
-        ll.add(88);
-        ll.print(); // <77, 88>
     }
 
 }
