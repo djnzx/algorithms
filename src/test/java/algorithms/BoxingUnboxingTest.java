37e143b45a8e4fcee95a47299d698c27058c3a89
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-02 17:57:09 +0400

    - .gitignore - pom.xml - JUnit5 setup - boxing/unboxing example

---
+++ src/test/java/algorithms/BoxingUnboxingTest.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoxingUnboxingTest {
+
+  private BoxingUnboxing bx;
+
+  @BeforeEach
+  void setup() {
+    this.bx = new BoxingUnboxing();
+  }
+
+  @Test
+  void boxing() {
+    assertEquals(new Integer(5), bx.boxing(5));
+  }
+
+  @Test
+  void unboxing() {
+    assertEquals(5, bx.unboxing(new Integer(5)));
+  }
+
+  @Test
+  void fail() {
+    assertEquals(true, false);
+  }
+}
