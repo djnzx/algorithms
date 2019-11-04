83b9a3d850449db0bd3a62d9f768b293d4be71e4
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-04 20:02:22 +0400

    -- exceptions polished

--- src/main/java/algorithms/sort/MergeSortCustomComparator.java
+++ src/main/java/algorithms/sort/MergeSortCustomComparator.java
@@ -1,14 +1,16 @@
 package algorithms.sort;
 
+import java.util.Comparator;
+
 /**
+ * Merge sort implementation
+ * complexity: O(n * log(N))
+ *
  * https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Merge_sort_algorithm_diagram.svg/300px-Merge_sort_algorithm_diagram.svg.png
  */
-public class MergeSortExplained {
+public class MergeSortCustomComparator {
 
-  void merge(int[] part, int idx_l, int idx_m, int idx_r) {
-    System.out.printf("MERGE: two arrays: %s and %s",
-        Utils.arrToString(part, idx_l, idx_m),
-        Utils.arrToString(part, idx_m + 1, idx_r));
+  void merge(int[] part, int idx_l, int idx_m, int idx_r, Comparator<Integer> cmp) {
     // calculating lengths
     int size_l = idx_m - idx_l + 1;
     int size_r = idx_r - idx_m;
@@ -30,7 +32,7 @@ public class MergeSortExplained {
     // initial index of merged sub-array
     int k = idx_l;
     while (i < size_l && j < size_r) {
-      if (left[i] <= right[j]) {
+      if (cmp.compare(left[i], right[j]) < 0) {
         part[k++] = left[i++];
       } else {
         part[k++] = right[j++];
@@ -46,33 +48,30 @@ public class MergeSortExplained {
     while (j < size_r) {
       part[k++] = right[j++];
     }
-    System.out.printf(" merged into: %s\n", Utils.arrToString(part, idx_l, idx_r));
   }
 
-  void sort(int[] data, int left, int right) {
+  void sort(int[] data, int left, int right, Comparator<Integer> cmp) {
     // left  - means index of the left part of array
     // right - means index of the right part of array
-    System.out.printf("SORT: within indexes: %d..%d, sub-array:%s\n", left, right,
-        Utils.arrToString(data, left, right));
-
     if (left < right) {
       int mid = (left + right) / 2;
-      sort(data, left, mid);
-      sort(data, mid + 1, right);
-      merge(data, left, mid, right);
+      sort(data, left, mid, cmp);
+      sort(data, mid + 1, right, cmp);
+      merge(data, left, mid, right, cmp);
     }
   }
 
   public static void main(String[] args) {
     // create a random array
-    int[] data = Utils.create_random_data(8);
+    int[] data = Utils.create_random_data(10);
     // make a copy not to mutate original data
     int[] sorted = data.clone();
     // create the new instance of our class
-    MergeSortExplained app = new MergeSortExplained();
+    MergeSortCustomComparator app = new MergeSortCustomComparator();
     // running sort
-    app.sort(sorted, 0, data.length - 1);
+    app.sort(sorted, 0, data.length - 1, (i1, i2) -> i2 - i1);
 
+    // print
     System.out.println("Merge sort: complexity: O(n*log(N))");
     System.out.printf("Source array: %s\n", Utils.arrToString(data));
     System.out.printf("Sorted array: %s\n", Utils.arrToString(sorted));
