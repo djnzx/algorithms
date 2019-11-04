85e68a91822022b0778752cf1b4fb41c61711339
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-04 13:12:02 +0400

    -- formatting

--- src/main/java/algorithms/bitwise/RealExampleTaxi.java
+++ src/main/java/algorithms/bitwise/RealExampleTaxi.java
@@ -2,9 +2,9 @@ package algorithms.bitwise;
 
 public class RealExampleTaxi {
 
-    final static int MASK_BIG        = 0b00000001;
-    final static int MASK_NONSMOKING = 0b00000010;
-    final static int MASK_AIRCOND    = 0b00000100;
-    final static int MASK_ANIMALS    = 0b00001000;
+  final static int MASK_BIG        = 0b00000001;
+  final static int MASK_NONSMOKING = 0b00000010;
+  final static int MASK_AIRCOND    = 0b00000100;
+  final static int MASK_ANIMALS    = 0b00001000;
 
 }
