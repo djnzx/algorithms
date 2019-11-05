1d88334a6b23e14c93d64deb5c880efed881472a
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-05 07:24:29 +0400

    -- shoes task

---
+++ src/test/java/brackets/ShoesGroupsTest.java
@@ -0,0 +1,41 @@
+package brackets;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoesGroupsTest {
+
+  private ShoesGroups c;
+
+  @BeforeEach
+  void setUp() {
+    this.c = new ShoesGroups();
+  }
+
+  @Test
+  void calc1() {
+    assertEquals(4, c.calc("RLRRLLRLRRLL"));
+  }
+
+  @Test
+  void calc2() {
+    assertEquals(4, c.calc("RLLLRRRLLR"));
+  }
+
+  @Test
+  void calc3() {
+    assertEquals(1, c.calc("LLRLRLRLRLRLRR"));
+  }
+
+  @Test
+  void calc4() {
+    assertEquals(2,c.calc("LRLR"));
+  }
+
+  @Test
+  void calc5() {
+    assertEquals(3,c.calc("LRRRLRLLLR"));
+  }
+}
