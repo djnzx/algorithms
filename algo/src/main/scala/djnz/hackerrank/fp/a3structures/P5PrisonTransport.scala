41155024df16ef058b3f85db6dc8b81e11ad1ee4
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-11-01 09:33:45 +0200

    -- prison

--- algo/src/main/scala/djnz/hackerrank/fp/a3structures/P5PrisonTransport.scala
+++ algo/src/main/scala/djnz/hackerrank/fp/a3structures/P5PrisonTransport.scala
@@ -1,13 +1,6 @@
 package djnz.hackerrank.fp.a3structures
 
 import djnz.tools.ASuite
-import org.scalactic.ColCompatHelper
-import org.scalatest.ConfigMap
-import scala.collection.immutable.AbstractMap
-import scala.collection.immutable.AbstractSet
-import scala.collection.immutable.SeqMap
-import scala.collection.immutable.SortedMap
-import scala.collection.immutable.SortedSet
 
 /** https://www.hackerrank.com/challenges/prison-transport/problem
   * https://cp-algorithms.com/data_structures/disjoint_set_union.html
@@ -26,33 +19,37 @@ object P5PrisonTransport {
 
   def repack(linked: Map[Int, Set[Int]]): List[Set[Int]] = {
 
-    def extract(linked: Map[Int, Set[Int]], process: Set[Int]): (Map[Int, Set[Int]], Set[Int]) = {
+    def extractSubgroup(linked: Map[Int, Set[Int]], process: Set[Int]): (Map[Int, Set[Int]], Set[Int]) = {
 
-      def go(links: Map[Int, Set[Int]], group0: Set[Int]): (Map[Int, Set[Int]], Set[Int]) = {
-        val (linksReduced, groupWider) = group0
-          .foldLeft(links -> group0) {
-            case (a @ (linked, collected), gi) =>
-              linked.get(gi) match {
-                case None     => a
-                case Some(gj) => (linked - gi) -> (collected ++ gj)
-              }
-          }
-        if (links.size == linksReduced.size) linksReduced -> groupWider
-        else go(linksReduced, groupWider)
-      }
+      @scala.annotation.tailrec
+      def go(links: Map[Int, Set[Int]], group0: Set[Int]): (Map[Int, Set[Int]], Set[Int]) =
+        group0.foldLeft(links -> group0) {
+          case (a @ (linked, collected), gi) =>
+            linked.get(gi) match {
+              case None     => a
+              case Some(gj) => (linked - gi) -> (collected ++ gj)
+            }
+        } match {
+          // nothing extracted, we are done
+          case (reduced, group) if links.size == reduced.size => reduced -> group
+          // something is extracted, repeat
+          case (reduced, group)                               => go(reduced, group)
+        }
 
       go(linked, process)
     }
 
-    def go(linked: Map[Int, Set[Int]], acc: List[Set[Int]]): List[Set[Int]] = linked match {
+    /** one iteration pulls one group from the linked */
+    @scala.annotation.tailrec
+    def extractGroup(linked: Map[Int, Set[Int]], acc: List[Set[Int]]): List[Set[Int]] = linked match {
       case m if m.isEmpty => acc
       case m              =>
         val g0 = m.head match { case (x, xs) => xs + x }
-        val (linked2, group) = extract(m.tail, g0)
-        go(linked2, group :: acc)
+        val (linked2, group) = extractSubgroup(m.tail, g0)
+        extractGroup(linked2, group :: acc)
     }
 
-    go(linked, Nil)
+    extractGroup(linked, Nil)
   }
 
   def main(args: Array[String]): Unit = {
