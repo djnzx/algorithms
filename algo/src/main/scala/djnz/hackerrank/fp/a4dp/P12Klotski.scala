40eba88bbec0688f79e0dc5925f6fd33ffa95242
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-11-13 22:39:58 +0200

    -- chapter 4 klotski

--- algo/src/main/scala/djnz/hackerrank/fp/a4dp/P12Klotski.scala
+++ algo/src/main/scala/djnz/hackerrank/fp/a4dp/P12Klotski.scala
@@ -12,20 +12,19 @@ object P12Klotski {
     override def toString: String = s"$src $dst"
   }
   case class Move(name: String, change: Change) {
-    override def toString: String =
-      s"$name $change"
+    override def toString: String = s"$name $change"
   }
 
   trait Figure {
     def name: String
     def points: Set[Loc]
     lazy val origin: Loc = points.foldLeft(Loc(Int.MaxValue, Int.MaxValue)) { case (Loc(y, x), l) => Loc(y min l.y, x min l.x) }
-    private def move0(dy: Int, dx: Int): Figure = Figure.of(
+    private def doMove(dy: Int, dx: Int): Figure = Figure.of(
       name,
       points.map { case Loc(y, x) => Loc(y + dy, x + dx) }
     )
     private def move(dy: Int = 0, dx: Int = 0): (Figure, Move) = {
-      val f2 = move0(dy, dx)
+      val f2 = doMove(dy, dx)
       val src = origin
       val dst = Loc(origin.y + dy, origin.x + dx)
       f2 -> Move(name, Change(src, dst))
@@ -45,6 +44,7 @@ object P12Klotski {
   }
 
   case class Board(data: Set[Figure], w: Int, h: Int) {
+    lazy val itemWidth = data.head.name.length
     lazy val figures: Set[String] = data.map(_.name)
     def figure(c: String) = data.find(_.name == c).get
     def hasFigureAt(c: String, loc: Loc) = figure(c).at(loc)
@@ -57,7 +57,7 @@ object P12Klotski {
     override def toString: String =
       data.flatMap(f => f.points.map(l => l -> f.name)).toMap match {
         case field => (0 until h)
-            .map(y => (0 until w).map(x => field.getOrElse(Loc(y, x), '.')).mkString(" "))
+            .map(y => (0 until w).map(x => field.getOrElse(Loc(y, x), "." * itemWidth)).mkString(" "))
             .mkString("\n")
       }
   }
@@ -76,6 +76,15 @@ object P12Klotski {
       Board(data, w = raw.head.length, h = raw.length)
     }
   }
+
+  def showBoards(xs: Iterable[Board]) =
+    if (xs.isEmpty) println("<empty>")
+    else {
+      val s0 = Array.fill(xs.head.h)("    ")
+      xs.map(_.toString.split("\n"))
+        .foldLeft(s0) { case (a, xx) => (a zip xx).map { case (s1, s2) => s"$s1    $s2" } }
+        .foreach(println)
+    }
 
   /** moves ONE figure ONE stop ALL possible directions + onBoard + noOverlap */
   def moveOneFigOneStep(board: Board, c: String): Set[(Board, Move)] = {
@@ -114,52 +123,60 @@ object P12Klotski {
   }
 
   /** moves ALL figures ALL possible places + onBoard + noOverlap */
-  def nextStep(board: Board): Set[(Board, Move)] =
+  def nextMove(board: Board): Set[(Board, Move)] =
     board.figures.flatMap(f => moveOneFigAllPossible(board, f))
 
+  def shortest[A](xs: List[A], ys: List[A]): List[A] = if (xs.length < ys.length) xs else ys
+
   /** combine and pick shorter path */
-  def combine[A](base: Map[A, Int], increment: Map[A, Int]) =
-    increment.foldLeft(base) { case (base, (a, step)) =>
+  def combine[A](base: Map[A, List[Move]], increment: Map[A, List[Move]]) =
+    increment.foldLeft(base) { case (base, (a, path)) =>
       base.updatedWith(a) {
-        case None        => Some(step)
-        case Some(step0) => Some(step0 min step)
+        case None        => Some(path)
+        case Some(path0) => Some(shortest(path0, path))
       }
     }
 
-  def solve(board: Board, c: String, target: Loc): Int = {
+  def collect(solOpt: Option[List[Move]], sol: List[Move]) = solOpt match {
+    case None      => Some(sol)
+    case Some(old) => Some(shortest(old, sol))
+  }
 
-    def go(visited: Set[Board], toVisit: Map[Board, Int], solution: Int): Int = ???
-//      toVisit.headOption match {
-//        case None                                        => solution
-//        case Some((_, step)) if step >= solution         => go(visited, toVisit.tail, solution)
-//        case Some((b, _)) if visited.contains(b)         => go(visited, toVisit.tail, solution)
-//        case Some((b, step)) if b.hasFigureAt(c, target) => go(visited + b, toVisit.tail, solution min step)
-//        case Some((b, step))                             =>
-//          val inc = nextStep(b).map(b => b -> (step + 1)).toMap
-//          val toVisit2 = combine(toVisit, inc)
-//          go(visited + b, toVisit2, solution)
-//      }
+  def solve(board: Board, c: String, target: Loc): Option[List[Move]] = {
 
-    go(Set.empty, Map(board -> 0), Int.MaxValue)
+    def go(visited: Set[Board], toVisit: Map[Board, List[Move]], sol: Option[List[Move]]): Option[List[Move]] = {
+      pprint.log("visited:")
+      showBoards(visited)
+      pprint.log("toVisit:")
+      showBoards(toVisit.keys)
+      toVisit.headOption match {
+        case None                                                           => sol
+        case Some((b, path)) if b.hasFigureAt(c, target)                    => go(visited + b, toVisit.tail, collect(sol, path))
+        case Some((b, _)) if visited.contains(b)                            => go(visited, toVisit.tail, sol)
+        case Some((b, path)) if path.size >= sol.fold(Int.MaxValue)(_.size) => go(visited + b, toVisit.tail, sol)
+        case Some((b, path))                                                =>
+          val toVisit2 = nextMove(b).map { case (b0, m) => b0 -> (m :: path) }.toMap
+          go(visited + b, combine(toVisit.tail, toVisit2), sol)
+      }
+    }
+
+    go(Set.empty, Map(board -> List.empty), None)
   }
 
   def next() = scala.io.StdIn.readLine()
 
   def main(args: Array[String]): Unit = {
     val (rows, cols) = next().split(" ").map(_.toInt) match { case Array(r, c) => (r, c) }
-    val raw = (1 to rows).map { _ =>
-      next().split(" ").take(cols)
-    }.toArray
+    val raw = (1 to rows).map(_ => next().split(" ").take(cols)).toArray
     val f = next().trim
     val targetLoc = next().split(" ").map(_.toInt) match { case Array(y, x) => Loc(y, x) }
-
     val board = Board.parse(raw)
-    // debug
-    pprint.log(board)
-    pprint.log(f)
-    pprint.log(targetLoc)
-    val r = solve(board, f, targetLoc)
-    println(r)
+    solve(board, f, targetLoc) match {
+      case None           => println("no solutions")
+      case Some(solution) =>
+        println(solution.size)
+        solution.reverse.foreach(println)
+    }
   }
 
 }
@@ -186,16 +203,20 @@ class P12Klotski extends ASuite {
     val b = Board.parse(raw)
     println(b)
 
-    println("-" * 10)
+//    println("-" * 10)
+//
+//    moveOneFigOneStep(b, "C")
+//      .foreach { case (b, m) => println(s"$b\n$m") }
+//
+//    println("-" * 10)
+//
+//    moveOneFigAllPossible(b, "C")
+//      .foreach { case (b, m) => println(s"$b\n$m") }
 
-    moveOneFigOneStep(b, "C")
-      .foreach { case (b, m) => println(s"$b\n$m") }
+    val bs = moveOneFigAllPossible(b, "C")
+      .map(_._1)
 
-    println("-" * 10)
-
-    moveOneFigAllPossible(b, "C")
-      .foreach { case (b, m) => println(s"$b\n$m") }
-
+    showBoards(bs)
   }
 
   test("all possible next moves") {
@@ -205,7 +226,7 @@ class P12Klotski extends ASuite {
     ).map(_.split(""))
     val b = Board.parse(raw)
 
-    nextStep(b)
+    nextMove(b)
       .foreach { case (b, m) => println(s"$b\n$m") }
   }
 
