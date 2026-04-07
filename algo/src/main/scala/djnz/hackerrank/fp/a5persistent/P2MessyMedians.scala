b417968a9260327fee875cc0ae345a0cb4b84398
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-04-07 21:04:45 +0300

    5.2

--- algo/src/main/scala/djnz/hackerrank/fp/a5persistent/P2MessyMedians.scala
+++ algo/src/main/scala/djnz/hackerrank/fp/a5persistent/P2MessyMedians.scala
@@ -9,78 +9,94 @@ object P2MessyMedians {
 
   sealed abstract class Tree(val count: Int)
   case object Empty extends Tree(0)
-  case class Node(l: Tree, r: Tree, override val count: Int) extends Tree(count)
+  case class Node(l: Tree, r: Tree, override val count: Int) extends Tree(count) {
+    def withL(t: Tree) = copy(t, r, count + 1)
+    def withR(t: Tree) = copy(l, t, count + 1)
+  }
+  object Node {
+    def l(t: Tree) = Node(t, Empty, 1)
+    def r(t: Tree) = Node(Empty, t, 1)
+    def n(n: Int) = Node(Empty, Empty, n)
+  }
 
-  def update(tree: Tree, lo: Int, hi: Int, pos: Int): Tree =
-    if (lo == hi) Node(Empty, Empty, tree.count + 1)
+  // add element to the tree (by index)
+  def add(tree: Tree, lo: Int, hi: Int, idx: Int): Tree =
+    if (lo == hi) Node.n(tree.count + 1)
     else {
       val mid = (lo + hi) >> 1
       tree match {
-        case Empty         =>
-          if (pos <= mid) Node(update(Empty, lo, mid, pos), Empty, 1)
-          else Node(Empty, update(Empty, mid + 1, hi, pos), 1)
-        case Node(l, r, c) =>
-          if (pos <= mid) Node(update(l, lo, mid, pos), r, c + 1)
-          else Node(l, update(r, mid + 1, hi, pos), c + 1)
+        case Empty if idx <= mid             => Node.l(add(Empty, lo, mid, idx))
+        case Empty                           => Node.r(add(Empty, mid + 1, hi, idx))
+        case n @ Node(l, _, _) if idx <= mid => n.withL(add(l, lo, mid, idx))
+        case n @ Node(_, r, _)               => n.withR(add(r, mid + 1, hi, idx))
       }
     }
 
+  // get the index
   def kth(tree: Tree, lo: Int, hi: Int, k: Int): Int =
     if (lo == hi) lo
     else {
       val mid = (lo + hi) >> 1
 
-      val leftCount = tree match {
+      val lCount = tree match {
         case Node(l, _, _) => l.count
         case Empty         => 0
       }
 
       tree match {
-        case Node(left, right, _) =>
-          if (k <= leftCount) kth(left, lo, mid, k)
-          else kth(right, mid + 1, hi, k - leftCount)
-        case Empty                => ???
+        case Node(l, _, _) if k <= lCount => kth(l, lo, mid, k)
+        case Node(_, r, _)                => kth(r, mid + 1, hi, k - lCount)
+        case Empty                        => sys.error("we don't query empty tree")
       }
     }
 
-  case class State(history: Vector[Tree], coords: Vector[Int], rankOf: Map[Int, Int]) {
-    def m: Int = coords.size - 1 // last coord elem
+  case class History(private val items: Vector[Tree]) {
+    val last = items.last
+    def append(t: Tree) = History(items :+ t)
+    def lookup(index: Int) = items(index)
+  }
+  object History {
+    def init = History(Vector(Empty))
   }
 
-  def perform(state: State, q: Int, qIdx: Int): (State, Int) = {
+  def perform(input: InputData)(state: History, q: Int, qIdx: Int): (History, Int) = {
     val tree2 = q match {
-      case q if q > 0 => update(state.history.last, 0, state.m, state.rankOf(q))
-      case q          => state.history(qIdx + 1 + q)
+      case q if q > 0 => add(state.last, input.min, input.max, input.indexes(q))
+      case q if q < 0 => state.lookup(qIdx + 1 + q)
+      case _          => sys.error("0 is not allowed as input")
     }
 
-    val idx = kth(tree2, 0, state.m, (tree2.count + 1) / 2)
-    val median = state.coords(idx)
-    val state2 = state.copy(history = state.history :+ tree2)
+    val idx = kth(tree2, input.min, input.max, (tree2.count + 1) / 2)
+    val median = input.all(idx)
+    val state2 = state.append(tree2)
 
     (state2, median)
   }
 
-  def pack(queries: Vector[Int]): (Vector[Int], Map[Int, Int]) = {
-    // all distinc trees
+  case class InputData(all: Vector[Int], indexes: Map[Int, Int]) {
+    val min = 0
+    val max = all.size - 1
+  }
+
+  def pack(queries: Vector[Int]): InputData = {
     val all = queries
       .filter(_ > 0) // add
       .distinct
       .sorted
-    (all, all.zipWithIndex.toMap)
+    InputData(all, all.zipWithIndex.toMap)
   }
 
   def main(args: Array[String]): Unit = {
     val n = readLine().toInt
     val qs = Vector.fill(n)(readLine().toInt)
 
-    val (distinct, distinctIndexed) = pack(qs)
-    val state0 = State(Vector(Empty), distinct, distinctIndexed)
+    val input = pack(qs)
 
     val (_, medians) =
       qs.zipWithIndex
-        .foldLeft((state0, List.empty[Int])) {
+        .foldLeft((History.init, List.empty[Int])) {
           case ((state, medians), (q, idx)) =>
-            val (state2, median) = perform(state, q, idx)
+            val (state2, median) = perform(input)(state, q, idx)
             (state2, median :: medians)
         }
 
