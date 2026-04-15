faeb8dda9f08e5ca0642d57a4d4f315df8bef968
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-04-15 19:51:57 +0300

    6.13

--- algo/src/main/scala/djnz/hackerrank/fp/a6adhoc/P13PuzzleAndPc.scala
+++ algo/src/main/scala/djnz/hackerrank/fp/a6adhoc/P13PuzzleAndPc.scala
@@ -2,10 +2,11 @@ package djnz.hackerrank.fp.a6adhoc
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-import scala.io.StdIn.readLine
 
 /** https://www.hackerrank.com/challenges/puzzle-and-pc/problem = 80 */
 object P13PuzzleAndPc {
+
+  import scala.io.StdIn.readLine
 
   def pow(n: Int) = 1 << n
 
@@ -29,7 +30,7 @@ object P13PuzzleAndPc {
       }
   }
 
-  def mkTromino0(x0: Int, y0: Int, blocked: At): Tromino = {
+  def mkTromino(x0: Int, y0: Int, blocked: At): Tromino = {
     val ats = for {
       x <- x0 to x0 + 1
       y <- y0 to y0 + 1
@@ -38,19 +39,61 @@ object P13PuzzleAndPc {
     Tromino.fromSeq(ats)
   }
 
+  // a b
+  // c d
+  sealed trait Q
+  case object A extends Q
+  case object B extends Q
+  case object C extends Q
+  case object D extends Q
+
   def solve(x1: Int, y1: Int, x2: Int, y2: Int, blocked: At): List[Tromino] =
-    // if 2x2 -> mkTromino0
-    // else:
-    //  split on 4
-    //  put artificial tromino
-    //  solve 4
-    //  combine the result
-    ???
+    if (x2 - x1 == 2) List(mkTromino(x1, y1, blocked))
+    else {
+      // center coordinates
+      val xd = (x2 + x1) >> 1
+      val yd = (y2 + y1) >> 1
+
+      // artificial Tromino used as blocked for subsolutions
+      val artA = At(xd - 1, yd - 1)
+      val artB = At(xd, yd - 1)
+      val artC = At(xd - 1, yd)
+      val artD = At(xd, yd)
+
+      // pack parameters to use them later
+      val quads = List(
+        A -> ((x1, y1, xd, yd), artA),
+        B -> ((xd, y1, x2, yd), artB),
+        C -> ((x1, yd, xd, y2), artC),
+        D -> ((xd, yd, x2, y2), artD)
+      )
+
+      val blockedQuad: Q =
+        if (blocked.x < xd && blocked.y < yd) A
+        else if (blocked.x >= xd && blocked.y >= yd) D
+        else if (blocked.x < xd) C
+        else B
+
+      // eliminate blocked & collect only artificial
+      val cs = quads.collect { case (q, (_, art)) if q != blockedQuad => art }
+
+      val t0 = Tromino.fromSeq(cs)
+
+      val sols4 = quads
+        .flatMap {
+          case (q, ((xa, ya, xb, yb), art)) =>
+            val hole = if (q == blockedQuad) blocked else art
+            solve(xa, ya, xb, yb, hole)
+        }
+
+      t0 :: sols4
+
+    }
 
   def main(args: Array[String]): Unit = {
     val m = readLine.toInt
     val Array(y, x) = readLine.split(" ").map(_.toInt)
-    val blocked = At(x, y)
+    val blocked = At(x - 1, y - 1) // 1 based
     val size = pow(m)
 
     solve(0, 0, size, size, blocked)
@@ -76,7 +119,7 @@ class P13PuzzleAndPc extends AnyFunSuite with Matchers {
   }
 
   test("mkTromino0") {
-    mkTromino0(6, 10, At(7, 10)) shouldEqual Tromino(At(6, 10), At(6, 11), At(7, 11))
+    mkTromino(6, 10, At(7, 10)) shouldEqual Tromino(At(6, 10), At(6, 11), At(7, 11))
   }
 
   test("2^n") {
