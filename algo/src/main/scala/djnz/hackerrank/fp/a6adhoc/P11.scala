fb3b0001c4c4b486b3bbc50683ab177d6d1681cf
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-04-18 15:35:32 +0300

    6.11.

--- algo/src/main/scala/djnz/hackerrank/fp/a6adhoc/P11.scala
+++ algo/src/main/scala/djnz/hackerrank/fp/a6adhoc/P11.scala
@@ -1,6 +1,7 @@
 package djnz.hackerrank.fp.a6adhoc
 
 /** https://www.hackerrank.com/challenges/elementary-watson/problem = 200*/
+// pure Horn clause logic with explicit disequality constraints
 object P11 {
 
 }
